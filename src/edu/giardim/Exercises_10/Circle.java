fba1434d85d1153acf2bfd2d280cb494e1c50cde
Michael Giardina <mgiardina02@gmail.com>
2022-10-05 13:57:39 -0400

    Updated Exercises_10

--- src/edu/giardim/Exercises_10/Circle.java
+++ src/edu/giardim/Exercises_10/Circle.java
@@ -6,13 +6,41 @@ public class Circle {
     private double radius = 1.0;
     private Matrix2D pos = Matrix2D.makePoint2D(0, 0);
 
-    public double getRadius(){
-        return radius;
+    public Circle(){
+        //do nothing
     }
 
-    public void setRadius(double r){
-        if (r >= 0){
-            radius = r;
+    public Circle(double radius){
+        setRadius(radius);
+    }
+
+    public Circle(Matrix2D pos){
+        setCenter(pos);
+    }
+
+    public Circle(double radius, Matrix2D pos){
+        setRadius(radius);
+        setCenter(pos);
+    }
+    
+    public Circle(Circle other){
+        //This is okay
+        this.radius = other.radius;
+        //This is not okay
+        //this.pos = other.pos;
+        //Two references to the same data -> it may change
+        //instead
+        this.pos = new Matrix2D(other.pos);
+
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public void setRadius(double radius){
+        if (radius >= 0){
+            this.radius = radius;
         }
     }
 
@@ -22,5 +50,21 @@ public class Circle {
 
     public void setCenter(Matrix2D p){
         pos = new Matrix2D(p);
+    }
+
+    public double getArea(){
+        return Math.PI*radius*radius;
+    }
+
+    public double getPerimeter(){
+        return 2.0*Math.PI*radius;
+    }
+
+    public String toString(){
+        String s = "Circle with Radius " 
+        + this.radius + " at (" + pos.getValue(0,0)
+        + ", " + pos.getValue(1, 0) + ")";
+
+        return s;
     }
 }
