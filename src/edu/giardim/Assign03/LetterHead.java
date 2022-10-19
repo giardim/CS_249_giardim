a818f9908399956f47493be2217621e3960bf740
Michael Giardina <mgiardina02@gmail.com>
2022-10-19 16:06:57 -0400

    Worked on Assignment 3

--- src/edu/giardim/Assign03/LetterHead.java
+++ src/edu/giardim/Assign03/LetterHead.java
@@ -1,24 +1,103 @@
 package edu.giardim.Assign03;
 
+import java.util.*;
+
 public class LetterHead {
     
-    public LetterHead(String name, String [] sloganLines, char getBoundaryChar){
-        //do nothing
+    private String name;
+    private String [] sloganLines = new String[4];
+    private char boundaryChar;
+
+    public LetterHead (String name, String [] sloganLines, char boundaryChar){
+        this.name = name;
+        this.boundaryChar = boundaryChar;
+
+        for (int i = 0; i < sloganLines.length; ++i){
+            if (sloganLines[i] == null){
+                this.sloganLines[i] = " ";
+            }
+            else{
+                this.sloganLines[i] = sloganLines[i];
+            } 
+        }
     }
 
     public String getName(){
-        //return name
-    }
-
-    public char getBoundaryChar(){
-        //return boundary char
+        return name;
     }
 
     public String getSlogan(){
-        //do something
+        StringBuilder sb = new StringBuilder();
+        for (String s : sloganLines){
+            if (s != null){
+                sb.append(s);
+            }else{
+                sb.append(" ");
+            } 
+            sb.append("\n");
+        }
+        String slogan = sb.toString();
+        return slogan;
     }
 
-    public void setNames(String name){
-        //dosomething
+    public char getBoundaryChar(){
+        return boundaryChar;
     }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setBoundaryChar(char boundaryChar){
+        this.boundaryChar = boundaryChar;
+    }
+
+    public void setSlogan(String [] sloganLines){
+        for (int i = 0; i < sloganLines.length; ++i){
+            if (sloganLines[i] == null){
+                this.sloganLines[i] = " ";
+            }
+            else{
+                this.sloganLines[i] = sloganLines[i];
+            } 
+        }
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        int padding = 0;
+        for (int i = 0; i < 40; ++i){
+            sb.append(boundaryChar);
+        }
+        sb.append("\n" + boundaryChar + name);
+        padding = 38 - name.length();
+        for (int i = 0; i < padding; ++i){
+            sb.append(" ");
+        }
+        sb.append(boundaryChar + "\n" + boundaryChar);
+        for (int i = 0; i < 38; ++i){
+            sb.append(" ");
+        }
+        sb.append(boundaryChar + "\n");
+        for (int i = 0; i < 4; ++i){
+            sb.append(boundaryChar);
+            if (sloganLines[i] == null){
+                sb.append("");
+                padding = 38;
+            }else{
+                sb.append(sloganLines[i]);
+                padding = 38 - sloganLines[i].length();
+            }
+            for (int j = 0; j < padding; ++j){
+                sb.append(" ");
+            }
+            sb.append(boundaryChar);
+            sb.append("\n");
+        }
+        for (int i = 0; i < 40; ++i){
+            sb.append(boundaryChar);
+        }
+        String letter = sb.toString();
+        return letter;
+    }    
 }
--- src/edu/giardim/Assign03/LetterPrinter.java
+++ src/edu/giardim/Assign03/LetterPrinter.java
@@ -1,6 +1,14 @@
 package edu.giardim.Assign03;
 
+import java.util.*;
+
 public class LetterPrinter {
-    //start assign03
+    
+    public static void main (String [] args){
+        String [] test = {"Hello, world", "goodbye"};
+        LetterHead lh = new LetterHead("Michael", test, '*');
+        System.out.println(lh.toString());
+        
+    }
 }
  
