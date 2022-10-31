1256e44a8b7bd2a96b30c3ee5772ecfb7b0cad9f
Michael Giardina <mgiardina02@gmail.com>
2022-10-31 17:02:32 -0400

    Added documentation

--- src/edu/giardim/Assign04/CharImage.java
+++ src/edu/giardim/Assign04/CharImage.java
@@ -1,11 +1,22 @@
+/***********************************
+ * Program that creates a 2D array and fills it
+ *      with a specified character, checks its bounds,
+ *      and prints it out
+ * Author: Michael Giardina
+ * Date: 31 OCT 2022
+ * Language: Java (JDK 18)
+ **********************************/
+
 package edu.giardim.Assign04;
 
 public class CharImage {
+    //Set variables
     private int rowCnt;
     private int colCnt;
     private char fillChar;
     char [][] charArray = new char[rowCnt][colCnt];
     
+    //Constructor
     public CharImage(int rowCnt, int colCnt, char fillChar){
         this.rowCnt = rowCnt;
         this.colCnt = colCnt;
@@ -13,6 +24,7 @@ public class CharImage {
         clear();
     }
 
+    //Clears the 2D array to its original values
     public void clear(){
         this.charArray = new char [rowCnt][colCnt];
         for (int i = 0; i < rowCnt; ++i){
@@ -22,14 +34,17 @@ public class CharImage {
         }
     }
 
+    //Returns the number of rows
     public int getRowCnt(){
         return rowCnt;
     }
 
+    //Returns the number of columns
     public int getColCnt(){
         return colCnt;
     }
 
+    //Returns if the position is in the bounds of the array
     public boolean isValidPosition(int row, int col){
         if ((row < charArray.length && row >= 0) && (col < charArray[0].length && col >= 0)){
             return true;
@@ -37,6 +52,7 @@ public class CharImage {
         return false;
     }
 
+    //Returns the character at the specified bounds
     public char getPos(int row, int col){
         if (isValidPosition(row, col)){
             return charArray[row][col];
@@ -44,6 +60,7 @@ public class CharImage {
         return ' ';
     }
 
+    //Places the array at the specified bounds
     public boolean setPos(int row, int col, char c){
         if (isValidPosition(row, col)){
             charArray[row][col] = c;
@@ -52,10 +69,12 @@ public class CharImage {
         return false;
     }
 
+    //Returns a string of the default array
     public String toString(){
         return (rowCnt + " x " + colCnt + " CharImage (default: " + fillChar + ")");
     }
 
+    //Returns the a string that represents the 2D array
     public String getMapString(){
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < rowCnt; ++i){
